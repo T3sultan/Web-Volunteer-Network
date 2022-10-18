a53c00339a6a73878a1d6cb7ac9ecf37dc230201
“T3sultan” <incognito.soul011@gmail.com>
2022-10-19 00:08:51 +0600

    folder structure

--- firebase.init.js
+++ firebase.init.js
@@ -1,20 +1,13 @@
-// Import the functions you need from the SDKs you need
 import { initializeApp } from "firebase/app";
 import { getAuth } from "firebase/auth";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
-
-// Your web app's Firebase configuration
 const firebaseConfig = {
-  apiKey: "AIzaSyAiShRjSdliS0jfaAqO0HUzft1KBHDE5aU",
-  authDomain: "web-volunteer-network.firebaseapp.com",
-  projectId: "web-volunteer-network",
-  storageBucket: "web-volunteer-network.appspot.com",
-  messagingSenderId: "193673709746",
-  appId: "1:193673709746:web:b0cb3ec5a649b8db8d21ab",
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
 };
-
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const auth = getAuth(app);
 
---
+++ src/views/pages/AddEvent/AddEvent.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const AddEvent = () => {
+  return (
+    <div>
+      <h2>add event</h2>
+    </div>
+  );
+};
+
+export default AddEvent;
